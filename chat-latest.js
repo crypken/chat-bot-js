1aae444fddd61811417c2ea74cf9a3e22afa5a6b
Vellieswaran <47740563+vellies@users.noreply.github.com>
2023-06-07 12:56:46 +0530

    latest file

--- chat-latest.js
+++ chat-latest.js
@@ -68,7 +68,7 @@ async function initial() {
              ${settings.chatButton == "chat-button1" ? "" :
                 settings.chatButton == "chat-button3" ? "" :
                     settings.chatButton == "chat-button5" ? "" : settings.agentOnlineText  
-                }<svg width="20px" viewBox="0 0 24 24" fill="#000000" stroke="#000000" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Close_LG"> <path id="Vector" d="M21 21L12 12M12 12L3 3M12 12L21.0001 3M12 12L3 21.0001" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+                }<svg width="20px" viewBox="0 0 24 24" fill=${settings.iconFillColor} stroke=${settings.iconFillColor} xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Close_LG"> <path id="Vector" d="M21 21L12 12M12 12L3 3M12 12L21.0001 3M12 12L3 21.0001" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
             </span>
                                           
                        </div>`,
